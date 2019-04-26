6f78fc7af5134c44d098d8edd821c4de34c9d27b
uglaspelomundo <senac.douglas@gmail.com>
2019-04-26 20:59:30 -0300

    add

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://loiane.com';
+  cursoArgular: boolean = true;
+  urlImagem: any = 'http://lorempixel.com/400/200/nature/';
+
+  valorAtual: string = '';
+  valorSalvo;
+
+  isMouseOver: boolean = false;
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  botaoClicado() {
+    alert('Botao clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement> evento.target).value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
